589736637c19c5187920dcf6db47e0df20ef518a
CihanNscc <alcincihan@gmail.com>
2024-02-05 00:48:50 -0400

    all remaining func added

--- MediaPlayer/MainWindow.xaml.cs
+++ MediaPlayer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using System.Security.Cryptography;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,33 +21,48 @@ namespace MediaPlayer
         TagLib.File currentFile;
 
         private bool mediaPlayerIsPlaying = false;
+        private bool mediaPlayerIsStopped = true;
 
         public MainWindow()
         {
             InitializeComponent();
+            playingButton.IsEnabled = false;
+            tagsButton.IsEnabled = true;
+            playingWindow.Visibility = Visibility.Visible;
+            tagsWindow.Visibility = Visibility.Collapsed;
         }
 
         private void Open_File_Btn_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
-
             ofd.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
 
-            if (ofd.ShowDialog() == true) {
-                fileNameBox.Text = ofd.FileName;
+            if (ofd.ShowDialog() == true)
+            {
+                currentFile = TagLib.File.Create(ofd.FileName);
+
+                myMediaPlayer.Source = new Uri(ofd.FileName);
+                playingWindow.tagTitleDisplay.Text = ofd.FileName;
+                tagsWindow.tagTitleBox.Text = currentFile.Tag.Title;
+                tagsWindow.tagAlbumBox.Text = currentFile.Tag.Album;
+                tagsWindow.tagYearBox.Text = currentFile.Tag.Year.ToString();
             }
+        }
 
-            currentFile = TagLib.File.Create(ofd.FileName);
-
-            myMediaPlayer.Source = new Uri(ofd.FileName);
+        private void Show_Playing_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            playingButton.IsEnabled = false;
+            tagsButton.IsEnabled = true;
+            playingWindow.Visibility = Visibility.Visible;
+            tagsWindow.Visibility = Visibility.Collapsed;
         }
 
         private void Show_Tags_Btn_Click(object sender, RoutedEventArgs e)
         {
-            var year = currentFile.Tag.Year;
-            var title = currentFile.Tag.Title;
-
-            tagNameBox.Text = title;
+            playingButton.IsEnabled = true;
+            tagsButton.IsEnabled = false;
+            playingWindow.Visibility = Visibility.Collapsed;
+            tagsWindow.Visibility = Visibility.Visible;
         }
 
         private void Play_CanExecute(object sender, CanExecuteRoutedEventArgs e)
@@ -58,6 +74,8 @@ namespace MediaPlayer
         {
             myMediaPlayer.Play();
             mediaPlayerIsPlaying = true;
+            mediaPlayerIsStopped = false;
+            playingWindow.tagDisplayMessage.Text = "Playing";
         }
 
         private void Pause_CanExecute(object sender, CanExecuteRoutedEventArgs e)
@@ -69,17 +87,38 @@ namespace MediaPlayer
         {
             myMediaPlayer.Pause();
             mediaPlayerIsPlaying = false;
+            playingWindow.tagDisplayMessage.Text = "Paused";
         }
 
         private void Stop_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = (myMediaPlayer.Source != null) && (mediaPlayerIsPlaying == true);
+            e.CanExecute = (myMediaPlayer.Source != null) && (mediaPlayerIsStopped == false);
         }
 
         private void Stop_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             myMediaPlayer.Stop();
             mediaPlayerIsPlaying = false;
+            mediaPlayerIsStopped = true;
+            playingWindow.tagDisplayMessage.Text = "Stopped";
         }
+
+        public void Save_Tags()
+        {
+            myMediaPlayer.Close();
+            currentFile.Tag.Title = tagsWindow.tagTitleBox.Text;
+            currentFile.Tag.Album = tagsWindow.tagAlbumBox.Text;
+
+            if (int.TryParse(tagsWindow.tagYearBox.Text, out int year))
+            {
+                currentFile.Tag.Year = (uint)year;
+            }
+            else
+            {
+                MessageBox.Show("Invalid year format. Please enter a valid number.");
+            }
+
+            currentFile.Save();
+        } 
     }
 }
---
+++ MediaPlayer/TagsWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaPlayer
+{
+    /// <summary>
+    /// Interaction logic for TagsWindow.xaml
+    /// </summary>
+    public partial class TagsWindow : UserControl
+    {
+        public TagsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            tagTitleBox.IsReadOnly = false;
+            tagAlbumBox.IsReadOnly = false;
+            tagYearBox.IsReadOnly = false;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow is MainWindow mainWindow)
+            {
+                mainWindow.Save_Tags();
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
